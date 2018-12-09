e37d53eaf8a886f5c33667c717bc77fd1087dd6b
a.a.vasiljev <a.a.vasiljev@gmail.com>
2018-12-09 18:22:09 +0700

    ещё поправил liquibase скрипты

--- dal/src/main/db/changes/005.function_book.sql
+++ dal/src/main/db/changes/005.function_book.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 
 --changeset aavasiljev:5 endDelimiter:
-CREATE OR REPLACE FUNCTION book(show_id INT, nums INT[])
+CREATE OR REPLACE FUNCTION book(show_id INT, nums INT[], OUT result BOOLEAN)
   RETURNS BOOLEAN
 AS
 $body$
@@ -13,12 +13,14 @@ BEGIN
   FOREACH num IN ARRAY nums
     LOOP
       IF (get_bit(curr, num - 1) = 1) THEN
-        RETURN false;
+        result = false;
+        RETURN;
       END IF;
       curr = set_bit(curr, num - 1, 1);
     END LOOP;
   UPDATE shows SET seats = curr WHERE id = show_id;
-  RETURN true;
+  result = true;
+  RETURN;
 END;
 $body$
   LANGUAGE PlPgSQL;
