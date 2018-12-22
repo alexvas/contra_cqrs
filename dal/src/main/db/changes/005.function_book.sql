fa8359763651fa3b632872053ec961e661772edb
a.a.vasiljev <a.a.vasiljev@gmail.com>
2018-12-22 08:45:08 +0700

    перенёс проверку, не заняты ли места, внутрь update-запроса
    в уровне изоляции транзакций read_committed, который используется в
    проекте, update-запросы выполняются как атомарные, включая проверку
    своего условия. Это как будто аналог оптимистического лока по oid.
    
    Если делать проверку в select отдельно от update, то гарантии на
    атомарность нет и при конкурентных записях в БД можно одновременно
    "занять" одно и тоже место двумя разными запросами.

--- dal/src/main/db/changes/005.function_book.sql
+++ dal/src/main/db/changes/005.function_book.sql
@@ -4,24 +4,28 @@
 
 --changeset aavasiljev:5 endDelimiter:
 CREATE OR REPLACE FUNCTION book(show_id INT, nums INT[], OUT result BOOLEAN)
-  RETURNS BOOLEAN
 AS
 $body$
 DECLARE
-  curr bit(100);
-  num  INT;
+  zeros   bit(100);
+  curr    bit(100);
+  num     INT;
+  updated INT;
 BEGIN
-  SELECT seats INTO curr FROM shows WHERE id = show_id;
+  zeros = B'0'::bit(100);
+  curr = zeros;
   FOREACH num IN ARRAY nums
     LOOP
-      IF (get_bit(curr, num - 1) = 1) THEN
-        result = false;
-        RETURN;
-      END IF;
       curr = set_bit(curr, num - 1, 1);
     END LOOP;
-  UPDATE shows SET seats = curr WHERE id = show_id;
-  result = true;
+  WITH rowCount AS (
+    UPDATE shows SET seats = seats | curr
+      WHERE id = show_id AND seats & curr = zeros
+      RETURNING 1
+  )
+  SELECT count(*) INTO updated
+  from rowCount;
+  result = updated > 0;
   RETURN;
 END;
 $body$
